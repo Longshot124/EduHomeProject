83608b808268f24fb96a00e2e1d34e4e51103390
kamra <kamra@LAPTOP-M49P7DNF>
2022-12-04 17:02:42 +0400

    fix some bugs

---
+++ Edu_Home/Controllers/TeacherController.cs
@@ -0,0 +1,30 @@
+﻿using Edu_Home.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Edu_Home.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly EduDbContext _eduDbContext;
+
+        public TeacherController(EduDbContext eduDbContext)
+        {
+            _eduDbContext = eduDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var teacher = await _eduDbContext.Teachers.Where(e => !e.IsDeleted).ToListAsync();
+            return View(teacher);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var teacher= await _eduDbContext.Teachers.FirstOrDefaultAsync(e=>e.Id==id);
+            if (teacher.Id == null) return NotFound();
+            return View(teacher);
+        } 
+    }
+}
---
+++ Edu_Home/ViewComponents/TeacherViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Edu_Home.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Edu_Home.ViewComponents
+{
+    public class TeacherViewComponent : ViewComponent
+    {
+        private readonly EduDbContext _eduDbContext;
+
+        public TeacherViewComponent(EduDbContext eduDbContext)
+        {
+            _eduDbContext = eduDbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var teacher = await _eduDbContext.Teachers.Where(e => !e.IsDeleted).ToListAsync();
+            return View(teacher);
+        }
+    }
+}
