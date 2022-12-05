45fa655ebf66bb81e0c808e2a8fb8fc418dc8b7a
kamra <kamra@LAPTOP-M49P7DNF>
2022-12-05 19:23:45 +0400

    Finish

--- Edu_Home/Controllers/HomeController.cs
+++ Edu_Home/Controllers/HomeController.cs
@@ -1,22 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Edu_Home.DAL;
+using Edu_Home.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Edu_Home.Controllers
 {
     public class HomeController : Controller
     {
-        
 
-        public HomeController(ILogger<HomeController> logger)
+
+        private readonly EduDbContext _eduDbContext;
+        public HomeController(EduDbContext eduDbContext)
         {
-            
+            _eduDbContext = eduDbContext;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var slider = await _eduDbContext.Sliders.ToListAsync();
+            var homeViewModel = new HomeViewModel
+            {
+                Sliders = slider,
+            };
+            return View(homeViewModel);
         }
 
-        
+
     }
 }
